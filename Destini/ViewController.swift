cacdd1a2e64b8db69b4dd6ae442cd94c9918d3fe
Georgi Sirakov <georgi.sirakov74@gmail.com>
2023-01-05 15:53:09 +0200

    Initial Commit

---
+++ Destini/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Destini
+//
+//  Created by Georgi Sirakov on 5.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
